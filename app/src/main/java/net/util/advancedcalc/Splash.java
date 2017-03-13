3ea8b5601aab62ee41f755c4cb62c5854e1f8b79
Parth <parth.parth48@gmail.com>
2017-03-13 14:49:03 +0530

    Initial commit

---
+++ app/src/main/java/net/util/advancedcalc/Splash.java
@@ -0,0 +1,35 @@
+package net.util.advancedcalc;
+
+import net.util.advancedcalc.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by sam on 6/11/13 Splash Screen show up when the application is
+ * starting
+ */
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle Calcu) {
+		super.onCreate(Calcu);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(5000);// makes the application to sleep for 5 Seconds
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent openCalcu = new Intent("net.util.advancedcalc.SOWGCALC");
+					startActivity(openCalcu);
+				}
+			}
+		};
+		timer.start();
+
+	}
+}
